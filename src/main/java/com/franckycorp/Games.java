8c7025534c0d879cd3982425bdc3003a105907f0
Franck Pedron <franckpedrondev@gmail.com>
2023-08-22 22:08:57 +0200

    Build - Add pattern Observator

--- src/main/java/com/franckycorp/Games.java
+++ src/main/java/com/franckycorp/Games.java
@@ -2,16 +2,36 @@ package com.franckycorp;
 
 import com.franckycorp.controller.GameController;
 import com.franckycorp.games.HighCardGameEvaluator;
+import com.franckycorp.view.GameSwingPassiveView;
 import com.franckycorp.view.GameSwingView;
+import com.franckycorp.view.GameViewables;
 
 public class Games {
 
     public static void main(String[] args) {
 
+        GameViewables views = new GameViewables();
+
         GameSwingView gsv = new GameSwingView();
         gsv.createAndShowGUI();
 
-        GameController gc = new GameController(DeckFactory.makeDeck(DeckFactory.DeckType.Normal), gsv, new HighCardGameEvaluator());
+        views.addViewable(gsv);
+
+        for (int i = 0; i < 3; i++) {
+            GameSwingPassiveView passiveView = new GameSwingPassiveView();
+            passiveView.createAndShowGUI();
+
+            views.addViewable(passiveView);
+
+            // Sleep
+            try {
+            Thread.sleep(2500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        GameController gc = new GameController(DeckFactory.makeDeck(DeckFactory.DeckType.Normal), views, new HighCardGameEvaluator());
 //        GameController gc = new GameController(new Deck(), gsv, new LowCardGameEvaluator());
         gc.run();
     }
